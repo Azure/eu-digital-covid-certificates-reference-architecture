adabf50607861bf8ec41b66f1c4019c3c1064346
ben <git@benjamin.ie>
2021-09-15 18:32:22 +0100

    Added printing for all  needed URLs / information to get android apps running (#3)
    Signed-off-by: ben <git@benjamin.ie>

--- eudgc-ie/outputs.tf
+++ eudgc-ie/outputs.tf
@@ -1,4 +1,17 @@
 output "issuance_web_address" {
   value       = "https://dgca-issuance-web.${module.base_infra.dns_zone_name}"
-  description = "The web address where the issueance website can be accessed"
+  description = "The web address where the issuance website can be accessed"
+}
+output "issuance_service_url" {
+  value       = "https://dgca-issuance-service.${module.base_infra.dns_zone_name}"
+  description = "The url where the issuance backend can be accessed"
+}
+output "businessrule_service_url" {
+  value       = "https://dgca-businessrule-service.${module.base_infra.dns_zone_name}"
+  description = "The url where the business rule backend can be accessed"
+}
+
+output "verifier_service_url" {
+  value       = "https://dgca-verifier-service.${module.base_infra.dns_zone_name}"
+  description = "The url where the verifier service backend can be accessed"
 }
