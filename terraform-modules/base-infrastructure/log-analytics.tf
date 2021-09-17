e02a222d2ad764f743daa124597efcc7b99464e2
ben <git@benjamin.ie>
2021-09-17 13:10:26 +0000

    logic feedback

--- terraform-modules/base-infrastructure/log-analytics.tf
+++ terraform-modules/base-infrastructure/log-analytics.tf
@@ -165,7 +165,7 @@ locals {
 
 # Link AKS Log Analytics Workspace to Log Analytics Cluster
 resource "azurerm_log_analytics_linked_service" "log_analytics_linked_service" {
-  count               = var.enable_log_analytics_workspace && length(local.log_analytics_cluster_id) != 0 ? 1 : 0
+  count               = var.enable_log_analytics_workspace && var.log_analytics_cluster_id != null ? 1 : 0
   resource_group_name = azurerm_resource_group.rg.name
   workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace[0].id
   write_access_id     = var.log_analytics_cluster_id
